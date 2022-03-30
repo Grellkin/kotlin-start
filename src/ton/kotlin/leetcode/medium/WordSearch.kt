5e954deed42a38432d05e37f357ab845682c7b93
Варганов Антон Сергеевич <avarganov@usetech.ru>
2022-03-31 00:10:22 +0500

    Word search https://leetcode.com/problems/word-search/

--- src/ton/kotlin/leetcode/medium/WordSearch.kt
+++ src/ton/kotlin/leetcode/medium/WordSearch.kt
@@ -14,20 +14,50 @@ fun main() {
 
 class Solution {
 
-    var checkedLetter: Array<BooleanArray> = Array(1,  { i -> BooleanArray(1)})
-    var xBound: Int = 0
-    var yBound: Int = 0
-    var fWord: String = ""
-
     fun exist(board: Array<CharArray>, word: String): Boolean {
-        xBound = board.size
-        yBound = board[0].size
-        fWord = word
-        checkedLetter = Array(xBound) { BooleanArray(yBound) }
+        val xSize = board.size
+        val ySize = board[0].size
+        val checkedLetters = Array(xSize) { BooleanArray(ySize) }
+
+        //this is fun in fun declaration for later use
+        fun checkCross(i: Int, j: Int, board: Array<CharArray>, currentChar: Int): Boolean {
+            if (board[i][j] == word[currentChar]) {
+
+                if (currentChar == word.lastIndex) return true
+                checkedLetters[i][j] = true
+
+                //check top
+                if (i > 0 && !checkedLetters[i-1][j]) {
+                    val found = checkCross(i - 1, j, board, currentChar + 1)
+                    if (found) return true
+                }
+
+                //check right
+                if (j < ySize-1 && !checkedLetters[i][j+1]) {
+                    val found = checkCross(i, j+1, board, currentChar + 1)
+                    if (found) return true
+                }
+
+                //check bottom
+                if (i < xSize-1 && !checkedLetters[i+1][j]) {
+                    val found = checkCross(i+1, j, board, currentChar + 1)
+                    if (found) return true
+                }
+
+                //check left
+                if (j > 0 && !checkedLetters[i][j-1]) {
+                    val found = checkCross(i, j-1, board, currentChar + 1)
+                    if (found) return true
+                }
+
+                checkedLetters[i][j] = false
+            }
+            return false
+        }
 
         for (i in board.indices) {
             for (j in board[i].indices) {
-                val res = checkit(i, j, board, 0)
+                val res = checkCross(i, j, board, 0)
                 if (res) return true
             }
         }
@@ -35,40 +65,4 @@ class Solution {
         return false
     }
 
-
-    fun checkit(i: Int, j: Int, board: Array<CharArray>, currentChar: Int): Boolean {
-        if (board[i][j] == fWord[currentChar]) {
-
-            if (currentChar == fWord.lastIndex) return true
-            checkedLetter[i][j] = true
-
-
-            //check top
-            if (i > 0 && !checkedLetter[i-1][j]) {
-                val checkit = checkit(i - 1, j, board, currentChar + 1)
-                if (checkit) return true
-            }
-
-            //check right
-            if (j < yBound-1 && !checkedLetter[i][j+1]) {
-                val checkit = checkit(i, j+1, board, currentChar + 1)
-                if (checkit) return true
-            }
-
-            //check bottom
-            if (i < xBound-1 && !checkedLetter[i+1][j]) {
-                val checkit = checkit(i+1, j, board, currentChar + 1)
-                if (checkit) return true
-            }
-
-            //check left
-            if (j > 0 && !checkedLetter[i][j-1]) {
-                val checkit = checkit(i, j-1, board, currentChar + 1)
-                if (checkit) return true
-            }
-
-            checkedLetter[i][j] = false
-        }
-        return false
-    }
 }
